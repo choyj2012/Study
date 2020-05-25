ddf73c0769d22f80f2a7e750fc3965169530d5cb
choyj2012 <choyj2012@gmail.com>
2020-05-25 23:58:53 +0900

    Stack

--- Algorithm/Stack.cpp
+++ Algorithm/Stack.cpp
@@ -1,35 +1,57 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
 
-void CreateStack();
-void StackPush();
-void StackPop();
-void Top();
-void isEmpty();
-void isFull();
+template<class T>
+class NODE {
+private:
+	T data;
+	NODE* next;
+public:
+	NODE(T t) {
+		data = t;
+		next = NULL;
+	}
+};
 
+template<class T>
+class Stack {
+private:
+	NODE<T>* top;
+	NODE<T>* head;
+	int size;
+public:
+	Stack<T>() {
+		head = NULL;
+		size = 0;
+	}
+
+	~Stack<T>() {
+		if (head != NULL) delete[] head;
+	}
+
+	void Push(T s);
+	void Pop();
+	void Top();
+	void isEmpty();
+};
+
+template<class T>
+void Stack<T>::Push(T s) {
+	NODE<T> temp(s);
+	if (head == NULL)
+		head = &temp;
+	else {
+		top->next = &temp;
+		top = &temp;
+	}
+}
+
+template<class T>
+void Stack<T>::Pop() {
+
+}
 int main() {
 
-}
-
-void CreateStack() {
-
-}
-
-void StackPush() {
-
-}
-
-void StackPop() {
-
-}
-
-void Top() {
-
-}
-void isEmpty() {
-
-}
-
-void isFull() {
+	Stack<int>* s = new Stack<int>();
 
 }
