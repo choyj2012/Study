a7f7d331c65f00afe342aeef319eace47d02ac30
choyj2012 <choyj2012@gmail.com>
2020-05-26 03:39:09 +0900

    Stack by C++

--- Algorithm/Stack.cpp
+++ Algorithm/Stack.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 template<class T>
 class NODE {
-private:
-	T data;
-	NODE* next;
 public:
-	NODE(T t) {
-		data = t;
-		next = NULL;
+	T data;
+	NODE* next = nullptr;
+	NODE(T s) {
+		data = s;
 	}
 };
 
@@ -17,41 +16,88 @@ template<class T>
 class Stack {
 private:
 	NODE<T>* top;
-	NODE<T>* head;
 	int size;
 public:
 	Stack<T>() {
-		head = NULL;
+		top = nullptr;
 		size = 0;
 	}
 
 	~Stack<T>() {
-		if (head != NULL) delete[] head;
+		while (!isEmpty()) {
+			Pop();
+		}
 	}
 
 	void Push(T s);
 	void Pop();
-	void Top();
-	void isEmpty();
+	T Top();
+	bool isEmpty();
+	void PrintStack();
 };
 
 template<class T>
 void Stack<T>::Push(T s) {
-	NODE<T> temp(s);
-	if (head == NULL)
-		head = &temp;
+	NODE<T>* newNode = new NODE<T>(s);
+	if (top == nullptr)
+		top = newNode;
 	else {
-		top->next = &temp;
-		top = &temp;
+		newNode->next = top;
+		top = newNode;
 	}
+	size++;
 }
 
 template<class T>
 void Stack<T>::Pop() {
+	if (isEmpty()) {
+		cout << "Stack is empty" << endl;
+		return;
+	}
 
+	NODE<T>* temp;
+	temp = top;
+	top = top->next;
+	delete temp;
+	size--;
 }
+
+template<class T>
+T Stack<T>::Top() {
+	return top->data;
+}
+
+template<class T>
+bool Stack<T>::isEmpty() {
+	if (top == nullptr)
+		return true;
+	else 
+		return false;
+}
+
+template<class T>
+void Stack<T>::PrintStack() {
+	NODE<T>* ptr;
+	for (ptr = top; ptr != nullptr; ptr = ptr->next) {
+		cout << ptr->data << " ";
+	}
+	cout << "\n";
+}
+
 int main() {
-
 	Stack<int>* s = new Stack<int>();
-
+	
+	s->Push(1);
+	s->Push(2);
+	s->Push(3);
+	s->PrintStack();
+	cout << s->Top() << endl;
+	s->Pop();
+	s->Pop();
+	s->PrintStack();
+	s->Pop();
+	cout << s->isEmpty() << endl;
+	s->Pop();
+	delete s;
+	return 0;
 }
