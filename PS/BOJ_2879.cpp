262a289c0e485fb051e3b7a2e0db8e625fe9ecd5
choyj2012 <choyj2012@gmail.com>
2020-05-31 17:39:34 +0900

    add comments

--- PS/BOJ_2879.cpp
+++ PS/BOJ_2879.cpp
@@ -4,8 +4,8 @@
 #include <math.h>
 #define N 1010
 
-int be[N];
-int af[N];
+int be[N];	//현재befor
+int af[N];	//목표after
 int n, i;
 
 int main() {
@@ -23,7 +23,7 @@ int main() {
 	int sum = 0;
 	int now = 0;
 	
-	for (i = 1; i <= n; i++) {
+	for (i = 1; i <= n; i++) {	//iii)부호 바뀌는 경우
 		gap = af[i] - be[i];
 		if (now * gap < 0) {
 			sum += abs(now);
@@ -31,15 +31,15 @@ int main() {
 			continue;
 		}
 
-		if (abs(now) < abs(gap)) {
+		if (abs(now) < abs(gap)) {	//i)같은구간에서 증가하는경우
 			now = gap;
 		}
-		else if (abs(now) > abs(gap)) {
+		else if (abs(now) > abs(gap)) {	//ii)같은구간에서 감소하는경우
 			sum += (abs(now) - abs(gap));
 			now = gap;
 		}
 	}
-	sum += abs(now);
+	sum += abs(now);	//vi)for문끝나고 그리던 막대 끝내주기
 	printf("%d", sum);
 	return 0;
 }
