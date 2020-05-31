fc1f8bc23e214bc1ac8dae6d0723a93d0e5b8312
choyj2012 <choyj2012@gmail.com>
2020-05-31 16:22:23 +0900

    add comments

--- PS/BOJ_1918.cpp
+++ PS/BOJ_1918.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int oprank(char op) {
 	if (op == '*' || op == '/') return 1;
 	else if (op == '+' || op == '-') return 2; 
-	else return 3;
+	else return 3;		//'('
 }
 
 int main() {
@@ -20,17 +20,17 @@ int main() {
 	int len = strlen(str);
 	int i;
 	for (i = 0; i < len; i++) {
-		if (str[i] >= 'A' && str[i] <= 'Z')
+		if (str[i] >= 'A' && str[i] <= 'Z')	//알파벳일경우
 			printf("%c", str[i]);
-		else if (str[i] == ')') {
+		else if (str[i] == ')') {	//닫는괄호일경우
 				while (!v.empty() && v.back() != '(') {
 					printf("%c", v.back());
 					v.pop_back();
 				}
 				v.pop_back();
 		}
-		else if (str[i] == '(')v.push_back(str[i]);
-		else {
+		else if (str[i] == '(')v.push_back(str[i]);	//여는괄호일경우
+		else {	//연산자 (+, -, *, /)
 			while (!v.empty() && oprank(v.back()) <= oprank(str[i])) {
 				printf("%c", v.back());
 				v.pop_back();
