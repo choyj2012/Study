ab314ee41fc7a4b9ef325a29d4c4231c11c42f61
choyj2012 <choyj2012@gmail.com>
2020-06-07 01:58:03 +0900

    3111 푸는중

--- PS/BOJ_3111.cpp
+++ PS/BOJ_3111.cpp
@@ -1,5 +1,95 @@
 #include <stdio.h>
 #include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+
+	string str, tar;
+	cin >> tar;
+	cin >> str;
+	string answer = str;
+
+	string::iterator it = str.begin();
+	string::reverse_iterator rit = str.rbegin();
+
+	vector<char> stack1, stack2;
+
+	int i;
+	bool flag = false;
+
+	while (1) {
+		
+		for (; it != rit.base(); it++) {
+			stack1.push_back(*it);
+
+			string::reverse_iterator rit_tar = tar.rbegin();
+			vector<char>::reverse_iterator rit_stack = stack1.rbegin();
+			while (stack1.size() >= tar.size()) {
+				if (rit_tar == tar.rend()) {
+					for (i = 1; i <= tar.size(); i++) {
+						stack1.pop_back();
+					}
+					flag = true;
+					
+					break;
+				}
+				if (*rit_tar != *rit_stack) break;
+
+				rit_tar++;
+				rit_stack++;
+			}
+		}
+		string temp1(it, rit.base());
+		answer = temp1;
+		if (!flag) break;
+		flag = false;
+
+		for (; !flag && rit.base() != it; rit++) {
+			stack2.push_back(*rit);
+
+			string::reverse_iterator rit_tar = tar.rbegin();
+			vector<char>::reverse_iterator rit_stack = stack2.rbegin();
+			while (stack2.size() >= tar.size()) {
+				if (rit_tar == tar.rend()) {
+					for (i = 1; i <= tar.size(); i++) {
+						stack2.pop_back();
+					}
+					flag = true;
+					break;
+				}
+				if (*rit_tar != *rit_stack) break;
+
+				rit_tar++;
+				rit_stack++;
+			}
+		}
+		string temp2(it, rit.base());
+		answer = temp2;
+		if (!flag)break;
+		flag = false;
+	}
+	
+	vector<char>::iterator vi = stack1.begin();
+	while (vi != stack1.end()) {
+		cout << *vi++;
+	}
+	cout << answer << endl;
+	vi = stack2.begin();
+	while (vi != stack2.end()) {
+		cout << *vi++;
+	}
+	return 0;
+}
+
+/*
+시간초과
+#include <stdio.h>
+#include <iostream>
 #include <vector>
 #include <string>
 using namespace std;
@@ -58,3 +148,4 @@ int main() {
 	cout << str << endl;
 	return 0;
 }
+*/
