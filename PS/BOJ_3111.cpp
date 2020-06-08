250d85780abb310b95a5dab5412f12c2b7b1871a
choyj2012 <choyj2012@gmail.com>
2020-06-08 14:28:06 +0900

    3111 푸는중

--- PS/BOJ_3111.cpp
+++ PS/BOJ_3111.cpp
@@ -1,151 +1,63 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
-#include <vector>
 using namespace std;
-
 
 int main() {
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 
-	string str, tar;
-	cin >> tar;
-	cin >> str;
-	string answer = str;
-
-	string::iterator it = str.begin();
-	string::reverse_iterator rit = str.rbegin();
-
-	vector<char> stack1, stack2;
-
-	int i;
-	bool flag = false;
-
-	while (1) {
-		
-		for (; it != rit.base(); it++) {
-			stack1.push_back(*it);
-
-			string::reverse_iterator rit_tar = tar.rbegin();
-			vector<char>::reverse_iterator rit_stack = stack1.rbegin();
-			while (stack1.size() >= tar.size()) {
-				if (rit_tar == tar.rend()) {
-					for (i = 1; i <= tar.size(); i++) {
-						stack1.pop_back();
-					}
-					flag = true;
-					
-					break;
-				}
-				if (*rit_tar != *rit_stack) break;
-
-				rit_tar++;
-				rit_stack++;
-			}
-		}
-		string temp1(it, rit.base());
-		answer = temp1;
-		if (!flag) break;
-		flag = false;
-
-		for (; !flag && rit.base() != it; rit++) {
-			stack2.push_back(*rit);
-
-			string::reverse_iterator rit_tar = tar.rbegin();
-			vector<char>::reverse_iterator rit_stack = stack2.rbegin();
-			while (stack2.size() >= tar.size()) {
-				if (rit_tar == tar.rend()) {
-					for (i = 1; i <= tar.size(); i++) {
-						stack2.pop_back();
-					}
-					flag = true;
-					break;
-				}
-				if (*rit_tar != *rit_stack) break;
-
-				rit_tar++;
-				rit_stack++;
-			}
-		}
-		string temp2(it, rit.base());
-		answer = temp2;
-		if (!flag)break;
-		flag = false;
-	}
+	string str, word;
 	
-	vector<char>::iterator vi = stack1.begin();
-	while (vi != stack1.end()) {
-		cout << *vi++;
-	}
-	cout << answer << endl;
-	vi = stack2.begin();
-	while (vi != stack2.end()) {
-		cout << *vi++;
-	}
-	return 0;
-}
+	cin >> word;
+	int wlen = word.size();
 
-/*
-시간초과
-#include <stdio.h>
-#include <iostream>
-#include <vector>
-#include <string>
-using namespace std;
-
-int main() {
-
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-
-	string s, str;
-	cin >> s;
 	cin >> str;
-	int lens = s.size();
-	bool flag = 0;
-	while (1) {
+	int front = 0;
+	int back = str.size() - 1;
+	int i;
+	bool flag1 = false, flag2 = false;
 
-		string::iterator it;
-		for (it = str.begin(); !flag && it != str.end(); it++) {
-			string::iterator sit = s.begin();
-			string::iterator strit = it;
-			while (strit != str.end() && *sit == *strit) {
-				sit++;
-				strit++;
-
-				if (sit == s.end()) {	//지울거있음
-					str.erase(it, strit);
-					flag = true;
-					break;
+	while (front<=back) {
+		if (str.size() < wlen) break;
+		while (front <= back) {
+			string part = str.substr(front, wlen);
+			if (word == part) {
+				string p1 = str.substr(0, front);
+				string p2 = str.substr(front + wlen, str.size()-(front+wlen-1));
+				str = p1 + p2;
+				if (back >= front && back <= front + wlen - 1) {
+					back = back - wlen + 1;
 				}
+				else
+					back -= wlen;
+				if (front > 0) front--;
+				flag1 = 1;
+				break;
 			}
-			
+			front++;
 		}
-
-		if (!flag) break;
-		flag = false;
-
-		string::reverse_iterator rit;
-		for (rit = str.rbegin(); !flag && rit != str.rend(); rit++) {
-			string::reverse_iterator rsit = s.rbegin();
-			string::reverse_iterator rstrit = rit;
-			while (rstrit!=str.rend() && *rsit == *rstrit) {
-				rsit++;
-				rstrit++;
-
-				if (rsit == s.rend()) {
-					str.erase(rstrit.base(), rit.base());
-					flag = true;
-					break;
-				}
+		if (str.size() < wlen) break;
+		if (!flag1) break;
+		while (front <= back) {
+			string part = str.substr(back - wlen + 1, wlen);
+			if (word == part) {
+				string p1 = str.substr(0, back - wlen+1);
+				string p2 = str.substr(back + 1, str.size() - back);
+				str = p1 + p2;
+				if (front >= back - wlen + 1 && front <= back)
+					front = back - wlen - wlen;
+				if (front < 0) front = 0;
+				back = back - wlen + 1;
+				flag2 = 1;
+				break;
 			}
+			back--;
 		}
-
-		if (!flag)break;
-		flag = false;
+		if (!flag2) break;
 	}
+
 	cout << str << endl;
 	return 0;
 }
-*/
+
